4fac16f619f04741a883f53b219e61acd85e8c54
Yousef <72878758+yousef647@users.noreply.github.com>
2025-05-19 01:00:03 +0300

    Delete core directory

--- core/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from posts.models import Post
-
-def home(request):
-    posts = Post.objects.all().order_by("-created_at")
-    return render(request, "home.html", {"posts": posts})
